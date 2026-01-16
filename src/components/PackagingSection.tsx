5e241afa33ee16100f55efa1f67473175ed09832
mhmdikhsxn <sxncreative@gmail.com>
2026-01-16 15:17:45 +0700

    update: latest2

--- src/components/PackagingSection.tsx
+++ src/components/PackagingSection.tsx
@@ -1,6 +1,7 @@
 import bulkLooseImg from "@/assets/packaging-bulk-loose.png";
 import bulkPkgImg from "@/assets/packaging-bulk.png";
 import fullPkgImg from "@/assets/packaging-full.png";
+import MasterBox from "@/assets/masterbox.png";
 import { useLanguage } from "@/contexts/LanguageContext";
 import { Box, CheckCircle2, Layers, Package } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
@@ -86,32 +87,36 @@ const PackagingSection = () => {
 
           <div className={`transition-all duration-1000 delay-700 ${isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}>
             <div className="grid md:grid-cols-2 gap-8">
-              <CorporateCard className="p-8 border-l-4 border-l-gold">
+              <div>
                 <div className="overflow-hidden rounded-t-xl">
-                  <img className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-105" src={bulkPkgImg} alt="img" />
+                  <img className="w-full h-full transition-transform duration-300 ease-in-out hover:scale-105" src={MasterBox} alt="img" />
                 </div>
 
-                <br />
-                <h4 className="text-white font-bold text-xl mb-3 flex items-center gap-2">
-                  <Box className="w-5 h-5 text-gold" />
-                  {t("packaging.masterBox.title")}
-                </h4>
-                <ul className="space-y-1">
-                  <li className="flex items-start gap-3 text-gray-300">
-                    <CheckCircle2 className="w-5 h-5 text-gold flex-shrink-0 mt-0.5" />
-                    <span>{t("packaging.masterBox.sizes")}</span>
-                  </li>
-                  <li className="flex items-start gap-3 text-gray-300">
-                    <CheckCircle2 className="w-5 h-5 text-gold flex-shrink-0 mt-0.5" />
-                    <span>{t("packaging.masterBox.color")}</span>
-                  </li>
-                </ul>
-              </CorporateCard>
+                <CorporateCard className="p-8 border-l-4 border-l-gold">
+                  <br />
+                  <h4 className="text-white font-bold text-xl mb-3 flex items-center gap-2">
+                    <Box className="w-5 h-5 text-gold" />
+                    {t("packaging.masterBox.title")}
+                  </h4>
+                  <ul className="space-y-1">
+                    <li className="flex items-start gap-3 text-gray-300">
+                      <CheckCircle2 className="w-5 h-5 text-gold flex-shrink-0 mt-0.5" />
+                      <span>{t("packaging.masterBox.sizes")}</span>
+                    </li>
+                    <li className="flex items-start gap-3 text-gray-300">
+                      <CheckCircle2 className="w-5 h-5 text-gold flex-shrink-0 mt-0.5" />
+                      <span>{t("packaging.masterBox.color")}</span>
+                    </li>
+                  </ul>
+                </CorporateCard>
+              </div>
+
+              <div>
+                <div className="overflow-hidden rounded-t-xl">
+                  <img className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-105" src={MasterBox} alt="img" />
+                </div>
 
               <CorporateCard className="p-8 border-l-4 border-l-gold">
-                <div className="overflow-hidden rounded-t-xl">
-                  <img className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-105" src={bulkPkgImg} alt="img" />
-                </div>
                 <br />
                 <h4 className="text-white font-bold text-xl mb- flex items-center gap-2">
                   <Layers className="w-5 h-5 text-gold" />
@@ -128,6 +133,7 @@ const PackagingSection = () => {
                   </li>
                 </ul>
               </CorporateCard>
+              </div>
             </div>
           </div>
         </div>
